8a03568db5d29babfc9021c3551f74a263c2984f
lyokolux <lyokolux@protonmail.com>
2018-03-18 18:15:22 +0100

    Add function: random_cube

--- create_virtual_cube.py
+++ create_virtual_cube.py
@@ -14,10 +14,12 @@ P.S : Il peut surement être amélioré (je n'ai pas passé beaucoup de temps de
 
 """
 
+from random import choice, randrange
 from rubicks_cube import *
-from random import choice
 
 NUMBER_OF_FACES = 9
+MOVEMENTS = [r, ri, l, li, b, bi, d, di, f, fi, u, ui]
+ONE_MOVE = 1
 
 def create_virtual_cube():
     print("""
@@ -31,3 +33,14 @@ def create_virtual_cube():
     for n_face in range(1, NUMBER_OF_FACES+1):
         cube += [input(f"color face n°{n_face}: ").replace(' ', '').upper()]
     return cube
+
+def random_cube():
+    (face_1, face_2, face_3) = (['W'] * 9, ['B'] * 9, ['Y'] * 9)
+    (face_4, face_5, face_6) = (['O'] * 9, ['G'] * 9, ['R'] * 9)
+    cube = [face_1, face_2, face_3, face_4, face_5, face_6]
+
+    for loop in range(randrange(30, 50)): # How many iterations ?
+        MOVEMENTS[randrange(12)](cube, ONE_MOVE)
+    return cube
+
+print(random_cube())
