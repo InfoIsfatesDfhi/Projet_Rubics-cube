0dd97976940acd8c6995c64f688cbddb9f452a6a
johannchopin <31794680+johannchopin@users.noreply.github.com>
2018-03-18 15:37:43 +0100

    Update create_virtual_cube.py

--- create_virtual_cube.py
+++ create_virtual_cube.py
@@ -1,3 +1,18 @@
+"""
+
+Ce code permet de créer rapidement son Rubick's cube virtuel exploitable par le script "rubicks_cube.py". Il revoit une string de ce genre que l'on peut copier-coller :
+
+face_1 = ['W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W']
+face_2 = ['B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B']
+face_3 = ['Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y']
+face_4 = ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O']
+face_5 = ['G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G']
+face_6 = ['R', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'R']
+
+P.S : Il peut surement être amélioré (je n'ai pas passé beaucoup de temps dessus) alors lâchez vous ;)
+
+"""
+
 def create_virtual_cube():
     counter = 1
     face_counter = 0
